0f01ce6bf714639bbae201994fe3265befeea975
LuisCarlosEV <luiscarlos.e.v@gmail.com>
2023-03-29 13:30:19 +0200

    Clase Linq

---
+++ .net/Colecciones/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Programando.CSharp.ConsoleApp1
+{
+    internal class Program
+    {
+        ///<summary>
+        ///Método Main inicio del programa
+        ///</summary>
+        static void Main(string[] args)
+        {
+        }
+        public static void hasteble()
+        {
+            // Instanciar
+            var ht = new Hashtable();
+
+            // Eliminar todos los elementos
+            ht.Clear();
+
+            // Añadir elementos
+            ht.Add(1200, "Borja Cabeza");
+            ht.Add("ANATR", "Ana Trujillo");
+
+            // Número de elementos
+            Console.WriteLine($"Número de elementos: {ht.Count}");
+
+            // Eliminar un elemento
+            ht.Remove(1200);
+
+            // Recorrer el HashTable
+            foreach (var clave in ht.Keys) Console.WriteLine($"{clave}: {ht[clave]}");
+        }
+
+        public static void List()
+        {
+            // Instanciar
+            List<string> list = new List<string>();
+            List<string> list1 = new();
+            var list2 = new List<string>();
+
+            // Eliminar los elementos
+            list.Clear();
+
+            // Añadir elementos
+            list.Add("azul");
+            list.Add("verde");
+            list.Add("rosa");
+            list.Add("amarillo");
+
+            // Añadir elementos en una posición
+            list.Insert(4, "blanco");
+
+            // Añadir todos los elementos de otra colección
+            var colores = new string[] { "marron", "naranja", "negro", "violeta" };
+            list.AddRange(colores);
+
+            // Número de elementos del List
+            Console.WriteLine($"Número de elementos: {list.Count}");
+
+            // Eliminar elementos
+            list.Remove("azul");
+            list.RemoveAt(4);
+            list.RemoveRange(2, 2);
+
+            // Saber si un elemento esta contenido
+            Console.WriteLine($"Contiene el item rojo: {list.Contains("rojo")}");
+
+            // Orderna los elementos
+            list.Sort();
+
+            // Invertir el orden
+            list.Reverse();
+
+            // Convertir en un array de object -> object[] array = new array[10]
+            var nuevoArray = list.ToArray();
+
+            // Recorrer el List
+            foreach (var item in list)
+                Console.WriteLine($"{list.IndexOf(item)}# Item: {item}");
+
+            for (var i = 0; i < list.Count; i++)
+                Console.WriteLine($"{i}# {list[i]}");
+        }
+
+        public static void Dictionary()
+        {
+            // Instanciar
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            Dictionary<int, string> dic1 = new();
+            var dic2 = new Dictionary<int, string>();
+
+            // Eliminar todos los elementos
+            dic.Clear();
+
+            // Añadir elementos
+            dic.Add(1200, "Borja Cabeza");
+            dic.Add(1300, "Ana Trujillo");
+            dic.Add(1412, "José Guzman");
+
+            // Modificar un valor
+            dic[1300] = "Antonio Trujillo";
+
+
+            // Número de elementos
+            Console.WriteLine($"Número de elementos: {dic.Count}");
+
+
+            // Eliminar un elemento
+            dic.Remove(1200);
+
+            // Recorrer el diccionario
+            foreach (var clave in dic.Keys)
+                Console.WriteLine($"{clave}: {dic[clave]}");
+        }
+    }
+}
---
+++ .net/Programando.Csharp.ejercicios.consolapp1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+
+namespace Programando.Csharp.ejercicios.consolapp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
+			ConsultasBasicas();
+		}
+	
+		static void ConsultasBasicas()
+		{
+			// T-SQL: SELECT *From dbo.ListaProductos
+
+			// Métodos de LINQ
+			var r1a = DataLists.ListaProductos
+			.Select(r => r);
+
+			// Expresion LINQ
+			var r1b = from r in DataLists.ListaProductos
+					  select r;
+
+			foreach(Producto item in r1a) Console.WriteLine($"{item.Id} {item.Descripcion}");
+			Console.WriteLine(Environment.NewLine);
+
+			// T-SQL: SELECT Nombre *From dbo.ListaClientes
+
+			// Métodos de LINQ
+			var r2a = DataLists.ListaClientes
+			.Select(r => r.Nombre);
+
+			// Expresion LINQ
+			var r2b = from r in DataLists.ListaClientes
+					  select r.Nombre;
+
+			foreach (string item in r2b) Console.WriteLine($"{item}"); 
+			Console.WriteLine(Environment.NewLine);
+
+			// T-SQL: SELECT Id, Nombre *From dbo.ListaClientes
+
+			// Métodos de LINQ
+
+			var r3a = DataLists.ListaClientes
+			.Select(r => new { r.Id, r.Nombre });
+
+			// Expresion LINQ
+			var r3b = from r in DataLists.ListaClientes
+					  select new { r.Id, r.Nombre };
+
+			foreach (var item in r3b) Console.WriteLine($"{item.Id} {item.Nombre}");
+			Console.WriteLine(Environment.NewLine);
+
+			// T-SQL: SELECT Id AS Code, Nombre AS NombreCompleto *From dbo.ListaClientes
+			var r3c = DataLists.ListaClientes
+			.Select(r => new { Code = r.Id, NombreCompleto = r.Nombre });
+
+			foreach (var item in r3c) Console.WriteLine($"{item.Code} {item.NombreCompleto}");
+			Console.WriteLine(Environment.NewLine);
+
+			// T-SQL: SELECT Descripción FROM db.ListaProductos WHERE precio < 0.90
+
+			// Métodos de LINQ
+			var r4a = DataLists.ListaProductos
+				.Where(r => r.Precio < 0.90)
+				.Select(r => r.Descripcion);
+			// Expresion LINQ
+			var r4b = from r in DataLists.ListaProductos
+					  where r.Precio <	0.90
+					  select r.Descripcion;
+			foreach (var item in r4b) Console.WriteLine($"{item}");
+			Console.WriteLine(Environment.NewLine);
+
+			// T-SQL: SELECT Descripción FROM db.ListaProductos WHERE precio < 0.90 ORDER By Descripción (alfabeticamente)
+
+			// Métodos de LINQ
+			var r5a = DataLists.ListaProductos				//Ordenando en db
+				.Where(r => r.Precio < 0.90)
+				.OrderBy(r => r.Descripcion)
+				.Select(r => r.Descripcion);
+
+			var r5aa = DataLists.ListaProductos             //Ordenando en db
+			.Where(r => r.Precio < 0.90)
+			.OrderByDescending(r => r.Descripcion)
+			.Select(r => r.Descripcion);
+
+			var r5aaa = DataLists.ListaProductos				//Ordenando en PC
+			.Where(r => r.Precio < 0.90)
+			.Select(r => r.Descripcion)
+			.OrderBy(r => r);					// r es un string entonces no es necesario especificar "Descripción"
+
+			// Expresion LINQ
+			var r5b = from r in DataLists.ListaProductos
+					  where r.Precio < 0.90
+					  orderby r.Descripcion
+					  select r.Descripcion;
+
+			var r5bb = from r in DataLists.ListaProductos
+					  where r.Precio < 0.90
+					  orderby r.Descripcion descending
+					  select r.Descripcion;
+
+			var r5bbb = from r in DataLists.ListaProductos
+					   where r.Precio < 0.90
+					   select r.Descripcion.ToList().OrderBy(r =>r);
+
+			foreach (var item in r5b) Console.WriteLine($"{item}");
+			Console.WriteLine(Environment.NewLine);
+
+			// Otros Ejemplos
+			// Constains -> Contine; StarsWith -> Comienza; EndsWith -> Finaliza
+
+			var r6a = DataLists.ListaProductos
+				.Where(r => r.Descripcion.ToLower().EndsWith("boli"))
+				.Select(r => r);
+
+			var r6b = from r in DataLists.ListaProductos
+					  where r.Descripcion.ToLower().Contains("boli")
+					  select r;
+
+			// Count -> Cuenta los elementos
+			// Distinct -> Valor distinto
+			// Max -> Valor maximo
+			// Min -> valor minimo
+			// Sum -> Suma de valores
+			// Average -> media de los valores
+			// Aggregate -> Aplicar formula o metodo de agregacion
+
+			var r7a = DataLists.ListaProductos
+				.Where(r => r.Precio < 0.90)
+				.Count();
+
+			var r7b = DataLists.ListaProductos
+				.Count(r => r.Precio < 0.90);
+
+			var r7c = (from r in DataLists.ListaProductos
+					   where r.Precio < 0.90
+					   select r.Descripcion).Count();
+
+		}
+	}
+}
